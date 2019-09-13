88ca10d5ec171a831153a69c7a6bd63a9ecb898c
Michael Woelfel <mwoelfel@luc.edu>
2019-09-13 23:55:38 +0000

    passed tests

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -4,6 +4,52 @@ package edu.luc.cs.laufer.cs372.shapes
 
 object boundingBox {
   def apply(s: Shape): Location = s match {
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+
+    case Rectangle(_, _) =>
+      new Location(0, 0, s)
+
+    case Ellipse(_, _) =>
+      Location(-50, -30, Rectangle(100, 60))
+
+    case Location(x, y, shape) =>
+      {
+        val b = boundingBox(shape)
+        Location(x + b.x, y + b.y, b.shape)
+      }
+
+    case Group(shapes @ _*) => {
+      val locationList = shapes map (s1 =>
+        {
+          boundingBox(s1)
+        })
+      val s1 = locationList.foldLeft[Location](locationList.head)((a, c) => {
+        val b = boundingBox(c)
+        val wmax = Math.max(a.x + a.shape.asInstanceOf[Rectangle].width, b.x + b.shape.asInstanceOf[Rectangle].width)
+        val wmin = Math.min(a.x, b.x)
+        val width = (wmax - wmin)
+
+        val hmax = Math.max(a.y + a.shape.asInstanceOf[Rectangle].height, b.y + b.shape.asInstanceOf[Rectangle].height)
+        val hmin = Math.min(a.y, b.y)
+        val height = (hmax - hmin)
+
+        Location(a.x, a.y, (Rectangle(width, height)))
+
+      })
+
+      Location(s1.x, s1.y, s1.shape)
+
+    }
+
+  }
+
+  def size(s: Shape): Int = s match {
+    case Rectangle(_, _)   => 1
+    case Ellipse(_, _)     => 1
+    case Location(x, y, s) => size(s)
+    case Group(shapes @ _*) => {
+      val shapeList = shapes
+      shapeList.foldLeft(0)((a, s1) => size(s1) + a)
+    }
   }
 }
+
--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -5,11 +5,22 @@ package edu.luc.cs.laufer.cs372.shapes
   */
 sealed trait Shape
 
-case class Rectangle(width: Int, height: Int) extends Shape
+case class Rectangle(width: Int, height: Int) extends Shape {
+  require(height != null, "null height in Rectangle")
+}
+
+case class Ellipse(width: Int, height: Int) extends Shape {
+  require(height != null, "null height in Ellipse")
+}
 
 case class Location(x: Int, y: Int, shape: Shape) extends Shape {
-  require(shape != null, "null shape in location")
+  require(shape != null, "null shape in Location")
+}
+
+case class Group(shape: Shape*) extends Shape {
+  require(shape != null, "null shape in Group")
 }
 
 // TODO add missing case classes (see test fixtures)
 // TODO must include validity checking for constructor arguments
+
